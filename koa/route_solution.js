7d89645ed063099e9333f5153b1ccbf5b04a48ef
zhaoyou <zhaoyou.xt@gmail.com>
2016-02-24 22:52:11 +0800

    add koa two solution

--- koa/route_solution.js
+++ koa/route_solution.js
@@ -1,25 +1,30 @@
+
 var koa = require('koa');
+
 var app = koa();
 
-app.use(function* (next){
+app.use(function* (next) {
   if (this.path !== '/') {
     return yield next;
   }
+
   this.body = 'hello koa';
 });
 
-app.use(function* (next){
+app.use(function* (next) {
   if (this.path !== '/404') {
     return yield next;
   }
+
   this.body = 'page not found';
 });
 
-app.use(function* (next){
+app.use(function* (next) {
   if (this.path !== '/500') {
     return yield next;
   }
+
   this.body = 'internal server error';
 });
 
-app.listen(process.argv[2] || 3000);
+app.listen(process.argv[2]);
