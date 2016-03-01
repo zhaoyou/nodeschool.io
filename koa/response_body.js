2d27ac54a05684a90b318c062084e6cff80441fc
zhaoyou <zhaoyou.xt@gmail.com>
2016-03-01 22:12:18 +0800

    add koa response_body

---
+++ koa/response_body.js
@@ -0,0 +1,16 @@
+var koa = require('koa');
+var fs = require('fs');
+
+var app = koa();
+
+app.use(function* (next) {
+  if (this.path !== '/stream') { return yield next;}
+  this.body = fs.createReadStream(process.argv[3]);
+});
+
+app.use(function* (next) {
+  if (this.path !== '/json') { return yield next;}
+  this.body = { foo: 'bar' };
+});
+
+app.listen(process.argv[2]);
